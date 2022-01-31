16978a2d869535337de67495e1cda85e5697eab7
Bruno Borak <borak13@gmail.com>
2022-01-31 09:31:38 +0000

    Add basic function structure.

---
+++ script.js
@@ -0,0 +1,26 @@
+// Wait for the DOM to finish loading before running this game
+//Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        let buttons = document.getElementsByTagName("button");
+
+        for (let button of buttons) {
+            button.addEventListener("click", function() {
+                
+            })
+        }
+    }
+})
+
+
+function gamePlay()
+
+function compareNumbers()
+
+function increaseLuckyGuess()
+
+function increaseUnluckyGuess()
